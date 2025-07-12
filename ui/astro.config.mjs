0265c27d1d535bc367025a882223b90d08970060
jm27 <jme2791@gmail.com>
2025-07-12 15:46:40 -0400

    updated format

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [react()],
   output: "server",
   adapter: vercel({
-    includeFiles: ["templates/newsletter.mjml"],
+    includeFiles: ["src/templates/newsletter.mjml.js"],
     webAnalytics: {
       enabled: true,
     },
